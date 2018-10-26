9fde4a445b791efafd16a4703ef2d8b6efadc43a
Michi Oshima <moshima@lightkeeper.com>
2018-10-26 15:07:28 -0400

    Disabled custom theme for console mode

--- init.el
+++ init.el
@@ -104,18 +104,19 @@
 (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 
 ;; theme
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (atom-dark)))
- '(custom-safe-themes
-   (quote
-    ("e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(when (display-graphic-p)
+  (custom-set-variables
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(custom-enabled-themes (quote (atom-dark)))
+   '(custom-safe-themes
+     (quote
+      ("e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" default))))
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   ))
